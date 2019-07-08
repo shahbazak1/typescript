30a1d6ac6d3ef5a26588ea80303e31c4ae2d2efc
Shahbaz Khan <shahbaz.aslam@afiniti.com>
2019-07-08 19:01:09 +0500

    Completed Rudimental UI to implement encoding/decoding of ROT-13

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const rot13 = require('rot-13')
+const path = require('path');
 const express = require('express')
+const hbs = require('hbs');
+
+
 const app = express()
+
+
+const publicDirectoryPath = path.join(__dirname, './public');
+const viewsPath = path.join(__dirname, './templates/views');
+const partialsPath = path.join(__dirname, './templates/partials');
+
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
 
 
 app.use(express.json())
@@ -10,7 +29,9 @@ const encoder = (req,res,next)=>{
     const encodedInput = input.map((item)=>{
         return rot13(item)
     })
-    res.send(encodedInput)
+    const arrayToJSON = Object.assign({}, encodedInput)
+    const stringJSON = JSON.stringify(arrayToJSON)
+    res.json(stringJSON)
     next();
     res.end()
 }
@@ -20,11 +41,17 @@ const decoder = (req,res,next)=>{
     const decodedInput = input.map((item)=>{
         return rot13(item)
     })
-    res.send(decodedInput)
+    const arrayToJSON = Object.assign({}, decodedInput)
+    const stringJSON = JSON.stringify(arrayToJSON)
+    res.json(stringJSON)
     next();
     res.end()
 }
 
+
+app.get('',(req,res)=>{
+    res.render('index')
+})
 
 app.post('/encode', encoder)
 app.post('/decode', decoder)
@@ -35,3 +62,38 @@ app.post('/decode', decoder)
 app.listen(3000, ()=>{
     console.log('Server Running..!')
 })
+
+
+
+
+
+
+// app.get('/weather', (req,res)=>{
+
+//     if(!req.query.address){
+//         return  res.send({
+//              error: 'You must provide an address'
+//          })
+//      }
+
+//      geocode(req.query.address, (error,{latitude, longitude, location}={})=>{
+//         if(error){
+//             return res.send({ error })  //Fixed an error for this, return was missing, cannot send res twice in http responses
+//         }
+        
+//      forecast(latitude, longitude, (error, forecastData) => {
+//         if(error){
+//             return res.send({error})
+//         }
+
+//             res.send({
+//                 forecast: forecastData,
+//                 location,
+//                 address: req.query.address
+//             })
+//          })
+
+//      })
+
+
+// });
